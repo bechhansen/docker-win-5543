30bbadb8d6b23b785329406aa1eff6ae665ca4e4
Peer Bech Hansen <peer.bech.hansen@gmail.com>
2020-02-06 21:05:10 +0100

    Reproducing bug

--- Test.java
+++ Test.java
@@ -1,57 +1,38 @@
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.nio.file.attribute.FileTime;
 
 public class Test {
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
+    	long time = System.currentTimeMillis();
+    	
         File folder = new File("/tmp/test/deployment");
         folder.mkdirs();
+        
+        Path path = folder.toPath();
+        Path file1 = path.resolve(time + "-1.txt");
+        Path file2 = path.resolve(time + "-2.txt");
+        
+        file1 = Files.createFile(file1);
+        file2 = Files.createFile(file2);
 
-        File file = new File(folder, "file.txt");
-        file.createNewFile();
-
-        while (true) {
-            long deploymentTimestamp = getDeploymentTimestamp(folder);
-            System.out.println("Time: " + deploymentTimestamp);
-            Thread.sleep(500);
-        }
-
-    }
-
-    private static long getDeploymentTimestamp(File deploymentFile) {
-        if (deploymentFile.isDirectory()) {
-            // Scan for most recent file
-            long latest = deploymentFile.lastModified();
-            for (File child : listDirectoryChildren(deploymentFile)) {
-                long childTimestamp = getDeploymentTimestamp(child);
-                if (childTimestamp > latest) {
-                    latest = childTimestamp;
-                }
-            }
-            return latest;
+        FileTime lastModifiedTime = Files.getLastModifiedTime(file1);
+        
+        System.out.println("Setting the timestamp of file2 to " + lastModifiedTime.toMillis());
+        Files.setLastModifiedTime(file2, lastModifiedTime);
+        System.out.println("Getting the timestamp of file2 to " + Files.getLastModifiedTime(file2).toMillis());
+        
+        long file1Millis = Files.getLastModifiedTime(file1).toMillis();
+        long file2Millis = file2.toFile().lastModified();
+        
+        if (file1Millis == file2Millis) {
+        	System.out.println("Timestamp is equal - no bug is found");
         } else {
-            return deploymentFile.lastModified();
-        }
+        	System.out.println("Timestamp is not equal - It should... this is a bug");	
+        }  
     }
-
-    private static List<File> listDirectoryChildren(File directory) {
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(directory.toPath())) {
-            final List<File> result = new ArrayList<>();
-            for (Path entry : stream) {
-                result.add(entry.toFile());
-            }
-            return result;
-        } catch (SecurityException | IOException ex) {
-            ex.printStackTrace();
-            return Collections.emptyList();
-        }
-    }
-
 }
